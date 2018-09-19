77b5f92ae29c98a5c41eb7244865b8b80c159d80
Sky_Ice <738157465@qq.com>
2018-09-19 09:54:41 +0800

    节点

--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Layout from './components/Layout.vue'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+import IndexPage from './page/Index.vue'
+
+const routes = [
+  {
+    path: '/index',
+    component: IndexPage
+  },
+  {
+    path: '*',
+    redirect: '/index'
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  router,
+  components: {Layout},
+  template: '<Layout/>'
 })
---
+++ src/page/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="index">
+    index
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
